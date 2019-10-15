f636e43918520c0f3fc30f9aadbec9acf9a46042
uerm <44007997+uerm@users.noreply.github.com>
2019-10-15 12:16:40 +0200

    Add files via upload

--- MITDB.m
+++ MITDB.m
@@ -199,20 +199,44 @@ ylabel('Amplitude (mV)')
 title('Subject 48, Lead II')
 legend('DWT filtering','Location','Best')
 
-%%
+%% Cutting the filtered signal in segments - Lead I, all patients
 cnt = 1;
-ecg_segments = {};
+ecg_segments1 = {};
 for p = 1:length(y1)
-    patient = y1{1,p};
-    peaks = Data_128{1,p};
-    seg_lengths = peaks(:,end)-peaks(:,1)+1;
-    max_len = max(seg_lengths);
-    patient_segments = zeros(0,max_len);
-    for i = 1:length(peaks(:,1))
-        seg = patient(peaks(i,1):peaks(i,end));
-        pad = zeros(1,max_len-length(seg));
-        padded_seg = [seg,pad];
-        patient_segments(end+1,:) = padded_seg;
+    patient1 = y1{1,p};
+    peaks1 = Data_128{1,p};
+    seg_lengths1 = peaks1(:,end)-peaks1(:,1)+1;
+    max_len1 = max(seg_lengths1);
+    patient_segments1 = zeros(0,max_len1);
+    for i = 1:length(peaks1(:,1))
+        seg1 = patient1(peaks1(i,1):peaks1(i,end));
+        pad1 = zeros(1,max_len1-length(seg1));
+        padded_seg1 = [seg1,pad1];
+        patient_segments1(end+1,:) = padded_seg1;
     end
-    ecg_segments{end+1} = patient_segments;
+    ecg_segments1{end+1} = patient_segments1;
+end
+
+%% Cutting the filtered signal in segments - Lead II, all patients
+cnt = 1;
+ecg_segments2 = {};
+for p = 1:length(y2)
+    patient2 = y2{1,p};
+    peaks2 = Data_128{1,p};
+    seg_lengths2 = peaks2(:,end)-peaks2(:,1)+1;
+    max_len2 = max(seg_lengths2);
+    patient_segments2 = zeros(0,max_len2);
+    for i = 1:length(peaks2(:,1))
+        seg2 = patient2(peaks2(i,1):peaks2(i,end));
+        pad2 = zeros(1,max_len2-length(seg2));
+        padded_seg2 = [seg2,pad2];
+        patient_segments2(end+1,:) = padded_seg2;
+    end
+    ecg_segments2{end+1} = patient_segments2;
+end
+
+%% Construct tensor for each patient
+
+for i = 1:length(Data)
+    tensor{1,i} = cat(3,ecg_segments1{1,i},ecg_segments2{1,i});
 end
